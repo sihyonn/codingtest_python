ecb9a4c3c200f9b11951c37cdd21a32ca1710846
sihyonn <sihyeon982@naver.com>
2025-09-04 11:06:30 +0900

    refactor: programmers[모의고사] 조건문 중복 제거

--- brute_force/programmers/mock_exam.py
+++ brute_force/programmers/mock_exam.py
@@ -1,23 +1,15 @@
 
 
 def solution(answers):
-    student1 = [1, 2, 3, 4, 5] 
-    student2 = [2, 1, 2, 3, 2, 4, 2, 5] 
-    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
-    
+    students = [[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]] 
     scores = [0,0,0]
     
     for i, ans in enumerate(answers):
-        if ans == student1[i % len(student1)]:
-            scores[0] +=1
-        if ans == student2[i % len(student2)]:
-            scores[1] += 1
-        if ans == student3[i % len(student3)]:
-            scores[2] += 1
-            
-    max_score = max(scores)
+        for j, student in enumerate(students):
+          if ans == student[i % len(student)]:
+             scores[j] += 1
     
-    return [i+1 for i, score in enumerate(scores) if score == max_score]
+    return [i+1 for i, student in enumerate(scores) if student == max(scores)]
 
 
 # 테스트
